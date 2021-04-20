6f1d76e12c6c85390473353a22f966bf7daad4bb
Caleb Denio <cjdenio44@gmail.com>
2021-04-19 20:06:43 -0400

    Refactor dokkud

--- dokkud/main.go
+++ dokkud/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
@@ -16,8 +15,6 @@ import (
 )
 
 func handler(ctx context.Context, reply jsonrpc2.Replier, req jsonrpc2.Request) error {
-	// log.Printf("Got connection! Method = %s\n", req.Method())
-
 	if req.Method() != "command" {
 		return errors.New("unsupported method")
 	}
@@ -30,40 +27,26 @@ func handler(ctx context.Context, reply jsonrpc2.Replier, req jsonrpc2.Request) 
 
 	log.Printf("Request to execute command %s %s", params.Command.Exe, strings.Join(params.Command.Args, " "))
 
-	// log.Printf("Exe = %s, Args = %+v\n", params.Command.Exe, params.Command.Args)
-
 	cmd := exec.Command(params.Command.Exe, params.Command.Args...)
-	stderrPipe, err := cmd.StderrPipe()
+
+	stderr := []byte{}
+	stdout, err := cmd.Output()
+
 	if err != nil {
-		return err
-	}
-	stdoutPipe, err := cmd.StdoutPipe()
-	if err != nil {
-		return err
-	}
-	err = cmd.Start()
-	if err != nil {
-		return err
-	}
-	stdout, err := ioutil.ReadAll(stdoutPipe)
-	if err != nil {
-		return err
-	}
-	stderr, err := ioutil.ReadAll(stderrPipe)
-	if err != nil {
-		return err
-	}
-	err = cmd.Wait()
-	if err != nil {
-		return err
+		switch v := err.(type) {
+		case *exec.ExitError:
+			log.Printf("Error while running command: %s", v.Stderr)
+			stderr = v.Stderr
+		default:
+			log.Printf(err.Error())
+			return err
+		}
 	}
 
 	output := dokku.CommandOutput{
 		Stdout: string(stdout),
 		Stderr: string(stderr),
 	}
-
-	// log.Printf("Output: %+v\n", output)
 
 	err = reply(ctx, output, nil)
 	if err != nil {
