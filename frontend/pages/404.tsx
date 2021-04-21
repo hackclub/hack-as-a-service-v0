824a9fc8eade09d7f701e16eb0ee25c43143a8ec
Caleb Denio <cjdenio44@gmail.com>
2021-04-21 17:42:39 -0400

    Fix 404 page

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -1,19 +1,19 @@
 import Head from "next/head";
+import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Hack as a Service</title>
+        <title>Not Found | HaaS</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Coming Soon</h1>
+        <h1 className={styles.title}>Not Found</h1>
         <h5 className={styles.subtitle}>
-          Hack as a Service | A <a href="https://hackclub.com">Hack Club</a>{" "}
-          project
+          Would you like to go <Link href="/">back home</Link>?
         </h5>
       </main>
     </div>
