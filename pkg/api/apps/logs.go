7e1ba8035cffd7645bec12a7a2772eb6fe473e92
Caleb Denio <cjdenio44@gmail.com>
2021-04-26 19:53:21 -0400

    Finish log streaming logic

--- pkg/api/apps/logs.go
+++ pkg/api/apps/logs.go
@@ -2,7 +2,6 @@ package apps
 
 import (
 	"context"
-	"fmt"
 	"io"
 	"strings"
 
@@ -16,21 +15,25 @@ func handleGETLogs(c *gin.Context) {
 	dokku_conn := c.MustGet("dokkuconn").(dokku.DokkuConn)
 
 	app_name := c.Param("id")
-	app_id, err := dokku_conn.RunCommand(context.Background(), []string{"haas:cid", app_name})
+
+	// Get the app's container ID
+	cid, err := dokku_conn.RunCommand(context.Background(), []string{"haas:cid", app_name})
 	if err != nil {
 		c.JSON(500, gin.H{"status": "error", "message": err.Error()})
 		return
 	}
 
-	app_id = strings.TrimSpace(app_id)
+	cid = strings.TrimSpace(cid)
 
+	// Initialize a Docker API client
 	cli, err := client.NewClientWithOpts(client.FromEnv)
 	if err != nil {
 		c.JSON(500, gin.H{"status": "error", "message": "Error creating Docker client"})
 		return
 	}
 
-	log_stream, err := cli.ContainerLogs(context.Background(), app_id, types.ContainerLogsOptions{
+	// Open a log stream via the Docker API
+	log_stream, err := cli.ContainerLogs(context.Background(), cid, types.ContainerLogsOptions{
 		ShowStdout: true,
 		ShowStderr: true,
 		Follow:     true,
@@ -41,17 +44,40 @@ func handleGETLogs(c *gin.Context) {
 		return
 	}
 
+	// Make a channel to hold the log stream
+	log_chan := make(chan []byte)
+
+	// A channel to close the client stream when the Docker API disconnects
+	log_stream_closed := make(chan bool)
+
+	// Listen for new logs in a goroutine
+	go func() {
+		for {
+			logs := make([]byte, 100)
+			_, err := log_stream.Read(logs)
+			if err != nil {
+				log_stream_closed <- true
+				return
+			}
+
+			log_chan <- logs
+		}
+	}()
+
+	// Close the log stream when done
 	defer log_stream.Close()
 
+	clientGone := c.Writer.CloseNotify()
+
 	c.Stream(func(w io.Writer) bool {
-		logs := make([]byte, 100)
-		log_stream.Read(logs)
-		w.Write(logs)
-
-		fmt.Println("loop")
-
+		select {
+		case <-clientGone:
+			return false
+		case <-log_stream_closed:
+			return false
+		case logs := <-log_chan:
+			w.Write(logs)
+		}
 		return true
 	})
-
-	fmt.Println("woot")
 }
