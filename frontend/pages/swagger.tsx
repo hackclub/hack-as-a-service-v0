f88dbe465b410cf0cb2d623225a27f077666f446
anirudhb <anirudhb@users.noreply.github.com>
2021-04-29 15:11:54 -0700

    feat: fix frontend

--- frontend/pages/swagger.tsx
+++ frontend/pages/swagger.tsx
@@ -1,6 +1,20 @@
+import { GetStaticProps } from "next";
+import fs from "fs/promises";
+import path from "path";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
-export default function Swagger() {
-  return <SwaggerUI url={`${process.env.NEXT_PUBLIC_API_BASE}/swagger.yaml`} />;
+export default function Swagger({ spec }: { spec: string }) {
+  return <SwaggerUI spec={spec} />;
 }
+
+export const getStaticProps: GetStaticProps = async (_) => {
+  return {
+    props: {
+      spec: await fs.readFile(
+        path.join(__dirname, "../../swagger.yaml"),
+        "utf-8"
+      ),
+    },
+  };
+};
