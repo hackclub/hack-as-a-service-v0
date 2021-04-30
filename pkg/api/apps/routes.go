f48e0449f464f430c68a627881ef9e3efa8583ac
anirudhb <anirudhb@users.noreply.github.com>
2021-04-29 20:21:16 -0700

    feat: rename endpoints

--- pkg/api/apps/routes.go
+++ pkg/api/apps/routes.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 func SetupRoutes(r *gin.RouterGroup) {
 	r.POST("/", handlePOSTApp)
 	r.GET("/:id", handleGETApp)
+	r.POST("/:id/rename", handlePOSTRename)
 	r.GET("/:id/builds", handleGETBuilds)
 	r.GET("/:id/logs", handleGETLogs)
 	r.POST("/:id/deploy", handlePOSTDeploy)
