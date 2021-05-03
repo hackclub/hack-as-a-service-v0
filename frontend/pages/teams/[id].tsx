4103e6910dd3bd6538da4f9d082865b09c6d980f
Jason Antwi-Appah <hey@jasonaa.me>
2021-05-02 19:00:33 -0500

    feat: truncate balances

--- frontend/pages/teams/[id].tsx
+++ frontend/pages/teams/[id].tsx
@@ -244,7 +244,7 @@ export default function TeamPage() {
           description={team?.team.Users.length}
           sx={{ marginRight: "100px" }}
         />
-        <Field label="Expenses" description={`${expenses} HN`} />
+        <Field label="Expenses" description={`${expenses > 0 ? expenses.toFixed(2) : "<1"} HN`} />
       </Flex>
 
       <Flex mt="35px" sx={{ flexWrap: "wrap", alignItems: "flex-start" }}>
