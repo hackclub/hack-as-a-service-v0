df11fa4eb3a9c9fcd03d9cd64afa861ec70ab806
anirudhb <anirudhb@users.noreply.github.com>
2021-05-04 12:08:26 -0700

    feat: fixy fixy should build now

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,15 +3,17 @@ import { SWRConfig } from "swr";
 import fetchApi from "../lib/fetch";
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-const haasTheme = extendTheme(theme, {
-  forms: {
-    input: {
-      border: "2px solid grey",
+const haasTheme = extendTheme(theme as any, {
+  components: {
+    Input: {
+      baseStyle: {
+        border: "2px solid grey",
+      },
     },
-  },
-  images: {
-    avatar: {
-      boxShadow: "0 4px 12px 0 rgba(0,0,0,.1)",
+    Avatar: {
+      baseStyle: {
+        boxShadow: "0 4px 12px 0 rgba(0,0,0,.1)",
+      },
     },
   },
 });
