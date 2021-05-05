990366e1fd5021c4031a19bcf8dfb052033cf4ed
anirudhb <anirudhb@users.noreply.github.com>
2021-05-05 09:14:15 -0700

    feat: landing, index, error pages

--- frontend/pages/landing.tsx
+++ frontend/pages/landing.tsx
@@ -1,56 +1,47 @@
 import { Box, Button, Flex, Image, Text } from "@chakra-ui/react";
 import Head from "next/head";
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <Box backgroundColor="white" color="#000000" sx={{ minHeight: "100vh" }}>
+    <Box backgroundColor="white" color="#000000" minHeight="100vh">
       <Head>
         <title>Hack as a Service</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Flex
-        sx={{
-          alignItems: "center",
-          justifyContent: "space-between",
-          px: "4",
-          pt: "3",
-          position: "absolute",
-          width: "100%",
-        }}
+        alignItems="center"
+        justifyContent="space-between"
+        px={2}
+        pt={1}
+        position="absolute"
+        width="100%"
       >
         <Image src="/nav-vector.svg" />
         <Link href="/login">
-          <Button backgroundColor="#EC3750" px="4">
+          <Button backgroundColor="#EC3750" px={2}>
             Login
           </Button>
         </Link>
       </Flex>
-      <Box
-        className={styles.container}
-        sx={{
-          backgroundImage: "url(/landing-vector.svg)",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-        }}
+      <Flex
+        height="100vh"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        backgroundImage="url(/landing-vector.svg)"
+        backgroundRepeat="no-repeat"
+        backgroundPosition="center"
       >
-        <Text
-          as="h1"
-          sx={{
-            fontSize: "5.5rem",
-            textAlign: "center",
-            py: "6",
-          }}
-        >
+        <Text as="h1" fontSize="5.5rem" textAlign="center" py={3}>
           A managed platform for makers.
         </Text>
         <Link href="/dashboard">
-          <Button backgroundColor="#EC3750" px="4">
+          <Button backgroundColor="#EC3750" px={2}>
             Start Building
           </Button>
         </Link>
-      </Box>
+      </Flex>
     </Box>
   );
 }
