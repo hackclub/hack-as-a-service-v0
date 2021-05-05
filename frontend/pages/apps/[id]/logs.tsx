76636d4c692516d0258e9b94641b7454835e01d9
Caleb Denio <cjdenio44@gmail.com>
2021-05-05 16:18:27 -0400

    SSR for other app pages

--- frontend/pages/apps/[id]/logs.tsx
+++ frontend/pages/apps/[id]/logs.tsx
@@ -3,6 +3,10 @@ import { useEffect, useRef, useState } from "react";
 import AppLayout from "../../../layouts/app";
 import { Text, useColorMode } from "@chakra-ui/react";
 import Logs from "../../../components/Logs";
+import { GetServerSideProps } from "next";
+import fetchApi from "../../../lib/fetch";
+import { IApp, ITeam, IUser } from "../../../types/haas";
+import useSWR from "swr";
 
 interface ILog {
   stream: "stdout" | "stderr";
@@ -38,16 +42,26 @@ function useLogs(appId: string): { logs: ILog[]; error: string | undefined } {
   return { logs, error: undefined };
 }
 
-export default function AppDashboardPage() {
+export default function AppDashboardPage(props: {
+  user: { user: IUser };
+  app: { app: IApp };
+  team: { team: ITeam };
+}) {
   const router = useRouter();
   const { id } = router.query;
+
+  const { data: user } = useSWR("/users/me", { initialData: props.user });
+  const { data: app } = useSWR(`/apps/${id}`, { initialData: props.app });
+  const { data: team } = useSWR(() => "/teams/" + app.app.TeamID, {
+    initialData: props.team,
+  });
 
   const { colorMode } = useColorMode();
 
   const { logs } = useLogs(id as string);
 
   return (
-    <AppLayout selected="Logs">
+    <AppLayout selected="Logs" user={user.user} app={app.app} team={team.team}>
       <Logs
         logs={logs}
         keyer={(log) => log.log}
@@ -73,3 +87,38 @@ export default function AppDashboardPage() {
     </AppLayout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  try {
+    const [user, app] = await Promise.all(
+      ["/users/me", `/apps/${ctx.params.id}`].map((i) =>
+        fetchApi(i, { headers: ctx.req.headers as HeadersInit })
+      )
+    );
+
+    const team = await fetchApi(`/teams/${app.app.TeamID}`, {
+      headers: ctx.req.headers as HeadersInit,
+    });
+
+    return {
+      props: {
+        user,
+        app,
+        team,
+      },
+    };
+  } catch (e) {
+    if (e.url == "/users/me") {
+      return {
+        redirect: {
+          destination: "/login",
+          permanent: false,
+        },
+      };
+    } else {
+      return {
+        notFound: true,
+      };
+    }
+  }
+};
