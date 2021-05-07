7cad56dc6463f4fc05bd6f6c11aa8effd80097af
Caleb Denio <cjdenio44@gmail.com>
2021-05-07 10:45:38 -0400

    Fix linting

--- pkg/api/builds/logs.go
+++ pkg/api/builds/logs.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 	"github.com/hackclub/hack-as-a-service/pkg/api/util"
 	"github.com/hackclub/hack-as-a-service/pkg/db"
 	"github.com/hackclub/hack-as-a-service/pkg/dokku"
