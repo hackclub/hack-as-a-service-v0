0ef3e5fa4eefd2f86015b04f59ab0025eb31b54a
Jason Antwi-Appah <hey@jasonaa.me>
2021-05-11 12:05:40 -0500

    fix: use ssr to obtain user/app/team info

--- frontend/pages/apps/[id]/addons.tsx
+++ frontend/pages/apps/[id]/addons.tsx
@@ -6,9 +6,33 @@ import { Addon } from "../../../components/Addon";
 import { Flex } from "@chakra-ui/react";
 import { devAddons } from "../../../lib/dummyData";
 
-export default function AppAddonOverview() {
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+
+import useSWR from "swr";
+
+import { fetchSSR } from "../../../lib/fetch";
+import { IApp, ITeam, IUser } from "../../../types/haas";
+export default function AppAddonOverview(props: {
+  user: { user: IUser };
+  app: { app: IApp };
+  team: { team: ITeam };
+}) {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data: user } = useSWR("/users/me", { initialData: props.user });
+  const { data: app } = useSWR(`/apps/${id}`, { initialData: props.app });
+  const { data: team } = useSWR(() => "/teams/" + app.app.TeamID, {
+    initialData: props.team,
+  });
   return (
-    <AppLayout selected="Addons">
+    <AppLayout
+      selected="Addons"
+      user={user.user}
+      app={app.app}
+      team={team.team}
+    >
       <Flex>
         <Stat
           style={{ marginRight: "100px" }}
@@ -27,3 +51,34 @@ export default function AppAddonOverview() {
     </AppLayout>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  try {
+    const [user, app] = await Promise.all(
+      ["/users/me", `/apps/${ctx.params.id}`].map((i) => fetchSSR(i, ctx))
+    );
+
+    const team = await fetchSSR(`/teams/${app.app.TeamID}`, ctx);
+
+    return {
+      props: {
+        user,
+        app,
+        team,
+      },
+    };
+  } catch (e) {
+    if (e.url == "/users/me") {
+      return {
+        redirect: {
+          destination: "/login",
+          permanent: false,
+        },
+      };
+    } else {
+      return {
+        notFound: true,
+      };
+    }
+  }
+};
